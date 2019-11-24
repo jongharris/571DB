dff00af485f883b50977f7171ff2e5438f6cdec2
Jon Harris <jonharrisg@gmail.com>
2019-11-23 19:38:49 -0700

    Update.

---
+++ dbConnection.php
@@ -0,0 +1,16 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "";
+    
+    //establish the connection to the DB
+    $connection = mysqli_connect($server,$username,$password,$database);
+try {
+    $connection = mysqli_connect($server,$username,$password,$database);
+     if($connection) {
+    }
+} catch (Exception $errormsg) {
+    echo $errormsg->getMessage();
+}
+?>
