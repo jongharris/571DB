784de408e986feb4439ff63c5eb15e15a333e0b0
Kett <saintmorose@gmail.com>
2019-12-01 12:58:01 -0700

    got All Your radar chart working Base on the league average the heatmap data Are a work in progress i'll insert the code so it Belong To team and then i'll create a function to get Us there

--- html/lineGraphQueries.php
+++ html/lineGraphQueries.php
@@ -8,8 +8,17 @@
 			."(SELECT date(date) AS date, idgames, home, homePPOpps, awayPPOpps FROM NHLapiDB.games WHERE home = ".$teamID." OR away = ".$teamID.") "
 			."AS GAMES ON game=idgames GROUP BY date) AS DATA JOIN (SELECT game, team FROM NHLapiDB.shots WHERE shootout=false) "
 			."AS SHOTS ON game=idgames GROUP BY DATE;";
+		//echo $query;
 		return mysqli_query($connection, $query);
+		
+	}
 	
+	function radarGraphTeamAvgs($connection){
+		$query = "SELECT goals/gp AS avgGoals, shots/gp AS avgShots, pps/gp AS avgPPs " 
+				."FROM (select count(*) as goals from NHLapiDB.goals where shootout=false) AS GOALS, " 
+				."(select count(*) as shots from NHLapiDB.shots where shootout=false) AS SHOTS, "
+				."(select count(*) as gp, sum(homePPOpps)+sum(awayPPOpps) as pps from NHLapiDB.games) AS GP;";
+		return mysqli_fetch_assoc(mysqli_query($connection, $query));
 		
 	}
 ?>
