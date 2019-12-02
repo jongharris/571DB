effc14a5d41eb1688c6a1dc2dcdbd9914faddaaf
Kett <saintmorose@gmail.com>
2019-12-02 12:13:59 -0700

    Auto stash before rebase of "origin/master"

--- html/lineGraphQueries.php
+++ html/lineGraphQueries.php
@@ -58,10 +58,11 @@
 	}
 
 	function lineGraphPlayerQuery($connection, $playerID) {
-		$query = "SELECT date(date) as date, count(IF(scorer=".$playerID.", 1, NULL)) as goals, count(IF(scorer=".$playerID.", NULL, 1)) as assists 
-				FROM NHLapiDB.games as gametable JOIN (SELECT game, scorer, assist1, assist2 FROM NHLapiDB.goals as go WHERE scorer="
-				.$playerID." OR assist1 = ".$playerID." OR assist2 = ".$playerID.") 
-				AS goaltable ON gametable.idgames = goaltable.game Group by date;";
+		$query = "SELECT date, count(IF(scorer=".$playerID.", 1, NULL)) as goals, count(IF(scorer!=".$playerID." AND scorer IS NOT NULL, 1, NULL)) as assists
+					FROM (Select idgames, date(date) as date from NHLapiDB.games) as DATES JOIN
+					((Select game from NHLapiDB.gamesPlayedIn where player = ".$playerID.") as GP LEFT JOIN
+					(SELECT game, scorer, assist1, assist2 FROM NHLapiDB.goals WHERE scorer= ".$playerID." 
+					OR assist1 = ".$playerID." OR assist2 = ".$playerID.") AS POINTS ON GP.game=POINTS.game) ON idgames=GP.game GROUP BY date;";
 		$runGraph = mysqli_query($connection, $query);
 		$date = array();
 		$goals = array();
